96034199728aea1dee4892bd950800e328c43e19
zeteoh <teohzeloong12@gmail.com>
2024-09-08 23:47:13 +0800

    feat: added bento grids

---
+++ components/Grid.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
